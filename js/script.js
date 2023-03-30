197e9698aaf65df7f2a30fe0650cdc51670f36a2
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-03-30 14:53:03 +0200

    link additem

--- js/script.js
+++ js/script.js
@@ -3,20 +3,31 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-           listToDo: [
-            {
-                text:"Finire L'esercizio",
-                done: false
-            },
-            {
-                text:"Comprendere la lezione",
-                done: false
-            }
-            
-           ]
+            listToDo: [
+                {
+                    text: "Finire L'esercizio",
+                    done: false
+                },
+                {
+                    text: "Comprendere la lezione",
+                    done: false
+                }
+
+            ],
+
+            newItemText:'',
         }
     },
-    methods(){
-        
+    methods: {
+        addItem() {
+            const newItemText = this.newItemText.trim()
+            if (newItemText !== '') {
+                this.listToDo.push({
+                    text: newItemText,
+                    done: false
+                })
+               this.newItemText = ''
+            }
+        }
     }
 }).mount('#app')
